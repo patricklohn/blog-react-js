ed71cde9b568cc1a3bce47524aca7cdf99a4d01c
patricklohn <patricklohn3@gmail.com>
2025-02-10 00:23:37 -0300

    Criação de barra de navegação

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
         <div className='App'>
+          <NavBar/>
           <div className='container'>
             <Outlet/>
           </div>
