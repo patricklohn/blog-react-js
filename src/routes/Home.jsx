389da8a8b25a0c7ccfac02a184a040a88f10912f
patricklohn <patricklohn3@gmail.com>
2025-02-10 19:14:11 -0300

    Configurando a comunicação com API do JSONPlaceholder com axios

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,34 @@
 import React from 'react'
+import axios from "axios";
+import {useState, useEffect} from "react"; 
+import {Link} from "react-router-dom";
+import "./Home.css";
+
 
 const Home = () => {
+
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async() =>{
+
+    try {
+      
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
+
+      const data = response.data;
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  useEffect(()=>{
+
+    getPosts();
+
+  },[])
+
   return (
     <div>
       <h1>Home</h1>
