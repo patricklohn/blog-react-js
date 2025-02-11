5d747508a17f0f7ccedeb248b1479f5434a84b07
patricklohn <patricklohn3@gmail.com>
2025-02-11 12:24:56 -0300

    Ajuste e estilização na tela de error #2

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './ErrorPage.css'
+import {Link} from "react-router-dom";
+import NavBar from "../components/NavBar"
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Error</h1>
+    <div className="">
+      <NavBar/>
+    <div className='error-container'>
+      <h1>Houve um erro inesperado.</h1>
+      <p>Favor entrar em contato com os administradores do BLOG ou tentar retornar a HOME!</p>
+      <Link to={`/`} className='btn' id='btn-err'>Voltar para Home</Link>
+    </div>
     </div>
   )
 }
